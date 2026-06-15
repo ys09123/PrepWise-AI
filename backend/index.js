2a069683d2d22cb902254bf209ce5cafc7dad786
Ankita Ghosh <ghoshankita753@gmail.com>
2026-06-15 18:12:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ const app = express();
 
 //middlewares
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://ai-interview-simulator-inky-three.vercel.app"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
